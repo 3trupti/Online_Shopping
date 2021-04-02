600292855c5a61e1cdb1997bed289e055cc60b7b
3trupti <3truptibhuta@gmail.com>
2021-04-02 19:16:38 +0530

    new

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomePageComponent } from './components/home-page/home-page.component';
+import { RegisterComponent } from './components/register/register.component';
 
 @NgModule({
   declarations: [
     AppComponent,
+    HomePageComponent,
+    RegisterComponent,
     
   ],
   imports: [
