d9837926da62c902ccf39a259022ea4e8e284993
Suraj <psuraj145.sp@gmail.com>
2021-04-04 01:55:20 +0530

    Suraj created HomePage Demo Version

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,13 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import{ HomePageComponent } from './components/home-page/home-page.component'
 
 
 @NgModule({
   declarations: [
     AppComponent,
+    HomePageComponent
    
     
   ],
