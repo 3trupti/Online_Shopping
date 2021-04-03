240e21c64c00c387b1a1825b521768b66e219a99
viswanarendra <viswa.panati@lntinfotech.com>
2021-04-03 16:13:11 +0530

    CHG1_viswa

--- src/app/components/login/login.component.ts
+++ src/app/components/login/login.component.ts
@@ -6,10 +6,19 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./login.component.css']
 })
 export class LoginComponent implements OnInit {
+  username:string;
+  password:string;
 
   constructor() { }
 
   ngOnInit(): void {
+  }
+  LoginUser()
+  {
+    if(this.username=="Admin" && this.password=="Admin123")
+    {
+      console.log("Welcome Admin")
+    }
   }
 
 }
