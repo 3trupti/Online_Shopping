2c9de81e92dd4ff4805f5f853e957c20b80b2de7
Suraj <psuraj145.sp@gmail.com>
2021-04-04 02:49:01 +0530

    Mere Error Resolved

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,21 +4,15 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-<<<<<<< HEAD
-import{ HomePageComponent } from './components/home-page/home-page.component'
-=======
-import { RegisterComponent } from './Components/register/register.component';
->>>>>>> 64a6eddd14d45b1d3d2c6ea8b84d308217a67b90
+import{ HomePageComponent } from './components/home-page/home-page.component';
+import{ RegisterComponent } from './components/register/register.component';
 
 
 @NgModule({
   declarations: [
     AppComponent,
-<<<<<<< HEAD
-    HomePageComponent
-=======
+    HomePageComponent,
     RegisterComponent
->>>>>>> 64a6eddd14d45b1d3d2c6ea8b84d308217a67b90
    
     
   ],
