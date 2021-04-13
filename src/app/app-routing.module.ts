6ad7f32a647a5152461f9e3ef27cb1e7b4512e47
viswanarendra <viswa.panati@lntinfotech.com>
2021-04-13 16:02:47 +0530

    chgsincontroller

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,13 +3,15 @@ import { RouterModule, Routes } from '@angular/router';
 import { HomePageComponent } from './components/home-page/home-page.component';
 import { RegisterComponent } from './components/register/register.component';
 import{LoginComponent} from  './components/login/login.component';
+import { AdminProfileComponent} from './admin-profile/admin-profile.component';
  
 
 const routes: Routes= [
  {path:'Home',component:HomePageComponent,pathMatch:"full"},
   {path:'Home',component:HomePageComponent},
   {path:'Register',component:RegisterComponent},
-  {path:'Login',component:LoginComponent}
+  {path:'Login',component:LoginComponent},
+  {path:'AdminHome',component:AdminProfileComponent}
 
 
 
